f456d6c40c3c7dc431459cc7ce4982dad9d47690
Edward Z. Yang <ezyang@meta.com>
2025-12-11 11:22:24 -0800

    Preserve original stack trace when rethrowing exception
    Authored with claude code
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    ghstack-source-id: 6be41050642084c733067f33f2e476d66e8eb5c1
    Pull-Request: https://github.com/pytorch/pytorch/pull/170198

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -1935,6 +1935,14 @@ class UserDefinedExceptionObjectVariable(UserDefinedObjectVariable):
     def exc_type(self):
         return self.exc_vt.exc_type
 
+    @property
+    def python_stack(self):
+        return self.exc_vt.python_stack
+
+    @python_stack.setter
+    def python_stack(self, value):
+        self.exc_vt.python_stack = value
+
 
 class KeyedJaggedTensorVariable(UserDefinedObjectVariable):
     @staticmethod
